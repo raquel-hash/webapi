a624be933ef061addfeecc5fc6b80d51a1e066d9
Raquel <quelcaraquel1@gmail.com>
2024-06-05 09:25:00 -0400

    Documentando API con Swagger

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -19,22 +19,22 @@ public class WeatherForecastController : ControllerBase
     {
         _logger = logger;
 
-        if(ListWeatherForecast == null || !ListWeatherForecast.Any())
+        if (ListWeatherForecast == null || !ListWeatherForecast.Any())
         {
-                ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-                })
-                .ToList();
+            ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
         }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    [Route("Get/weatherForecast")]
-    [Route("Get/weatherForecast2")]
-    [Route("[action]")]
+    // [Route("Get/weatherForecast")]
+    // [Route("Get/weatherForecast2")]
+    // [Route("[action]")]
     public IEnumerable<WeatherForecast> Get()
     {
         _logger.LogDebug("Retornando la lista de WeatherForecast");
