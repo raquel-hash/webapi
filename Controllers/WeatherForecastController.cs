4c197174f8e5d5bd7270ec324ea4ba453c90230e
Raquel <quelcaraquel1@gmail.com>
2024-06-05 08:31:31 -0400

    Manejo de ruta

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace webapi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -32,6 +32,9 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
+    [Route("Get/weatherForecast")]
+    [Route("Get/weatherForecast2")]
+    [Route("[action]")]
     public IEnumerable<WeatherForecast> Get()
     {
         return ListWeatherForecast;
